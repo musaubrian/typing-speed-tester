06dc29a05af89da8d101a1e7cafdf9051ff691e3
Ernest_musau <musaubrian45@gmail.com>
2022-01-31 20:19:13 +0300

    fixed the bug in the reset funtion

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import random
 
 class TypingSpeedGui:
     def __init__(self):
-        self.root = tk.Tk();
+        self.root = tk.Tk()
         self.root.title("Typing Speed Application")
         self.root.geometry("800x600")
 
@@ -15,17 +15,20 @@ class TypingSpeedGui:
 
         self.frame = tk.Frame(self.root)
 
-        self.sample_label = tk.Label(self.frame, text = random.choice(self.texts), font=("Arial", 18))
+        self.sample_label = tk.Label(
+            self.frame, text=random.choice(self.texts), font=("Arial", 18))
         self.sample_label.grid(row=0, column=0, columnspan=2, padx=5, pady=10)
 
         self.input_entry = tk.Entry(self.frame, width=30, font=("Arial", 20))
         self.input_entry.grid(row=1, column=0, columnspan=2, padx=5, pady=10)
         self.input_entry.bind("<KeyRelease>", self.start)
 
-        self.speed_label = tk.Label(self.frame, text = "Speed: \n 0.00CPS\n 0.00CPM", font=("Arial", 18))
+        self.speed_label = tk.Label(
+            self.frame, text="Speed: \n 0.00CPS\n 0.00CPM", font=("Arial", 18))
         self.speed_label.grid(row=2, column=0, columnspan=2, padx=5, pady=10)
 
-        self.reset_button = tk.Button(self.frame, text="Reset", command=self.reset)
+        self.reset_button = tk.Button(
+            self.frame, text="Reset", command=self.reset)
         self.reset_button.grid(row=3, column=0, columnspan=2, padx=5, pady=10)
 
         self.frame.pack(expand=True)
@@ -48,23 +51,25 @@ class TypingSpeedGui:
 
         if self.input_entry.get() == self.sample_label.cget('text')[:-1]:
             self.isRunning = False
-            self.input_entry.config(fg="green")               
+            self.input_entry.config(fg="green")
 
     def reset(self):
-        self.isRunning =False
+        self.isRunning = False
         self.counter = 0
-        self.speed_label.config(text = "Speed: \n 0.00CPS\n 0.00CPM")
+        self.speed_label.config(text="Speed: \n 0.00CPS\n 0.00CPM")
         self.sample_label.config(text=random.choice(self.texts))
-        self.input_entry.delete(0, tk.End)
-
+        self.input_entry.delete(0, tk.END)
 
     def time_thread(self):
         while self.isRunning:
             time.sleep(0.1)
             self.counter += 0.1
-            CPS = len(self.input_entry.get()) /self.counter
+            CPS = len(self.input_entry.get()) / self.counter
             CPM = CPS * 60
-            self.speed_label.config(text=f"Speed: \n{CPS:.2F} CPS\n{CPM:.2F} CPM")    
+            WPS = len(self.input_entry.get().split(" ")) / self.counter
+            WPM = WPS * 60
+            self.speed_label.config(
+                text=f"Speed: \n{CPS:.2F} CPS\n{CPM:.2F} CPM\n{WPS:.2F} WPS\n{WPM:.2F} WPM")
 
 
 TypingSpeedGui()
